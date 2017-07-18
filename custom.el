b87fa2e0ed5888ba18bdfddc6172e833783209ba
Kári Tristan Helgason <kthelgason@gmail.com>
2017-07-18 10:26:51 +0200

    update custom vars

--- custom.el
+++ custom.el
@@ -3,13 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(c-offsets-alist (quote ((cpp-macro . [0]) (innamespace . 0))))
+ '(c-offsets-alist
+   (quote
+    ((statement-cont . ++)
+     (cpp-macro .
+                [0])
+     (objc-method-call-cont . ++)
+     (innamespace . 0))))
  '(custom-safe-themes
    (quote
     ("9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" default)))
+ '(flycheck-clang-language-standard "c++11")
  '(package-selected-packages
    (quote
-    (helm-smex spaceline projectile ag exec-path-from-shell web-mode flx elixir-mode evil-magit markdown-mode spaceline-config base16-themes base16-theme evil-leader magit gitconfig-mode gitignore-mode git-blamed whitespace-cleanup-mode highlight-escape-sequences undo-tree rainbow-delimiters nlinum unfill default-text-scale ivy smex flycheck anzu diff-hl dired-sort dired+ fullframe disable-mouse diminish)))
+    (spaceline projectile ag exec-path-from-shell web-mode flx elixir-mode evil-magit markdown-mode spaceline-config base16-themes base16-theme evil-leader magit gitconfig-mode gitignore-mode git-blamed whitespace-cleanup-mode highlight-escape-sequences undo-tree rainbow-delimiters nlinum unfill default-text-scale ivy smex flycheck anzu diff-hl dired-sort dired+ fullframe disable-mouse diminish)))
  '(rtags-process-flags "--compiler-wrappers \"gomacc\"")
  '(safe-local-variable-values
    (quote
